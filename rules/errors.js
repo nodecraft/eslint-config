f87f8bc7435cd189c84941f2be54d91342ff065e
James Ross <james@nodecraft.com>
2017-05-10 02:17:39 +0100

    1.0.5 release
    - Set `allowEmptyCatch` option on no-empty rule, previously inherited
    from eslint:recommended

--- rules/errors.js
+++ rules/errors.js
@@ -4,6 +4,11 @@ module.exports = {
 		// disallow the use of console
 		"no-console": "off",
 
+		// disallow empty block statements
+		"no-empty": ["error", {
+			"allowEmptyCatch": true
+		}],
+
 		// disallow null comparisons without type-checking operators
 		"no-eq-null": "error",
 	}
