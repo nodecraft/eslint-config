e4e4e4898f06119e92bda09637d6aca98d1b3995
James Ross <james@jross.me>
2021-10-17 17:54:48 +0100

    chore: release 12.0.0
    chore: disable `vuejs-accessibility/accessible-emoji`

--- rules/plugin-vuejs-accessibility.js
+++ rules/plugin-vuejs-accessibility.js
@@ -2,6 +2,9 @@
 
 module.exports = {
 	rules: {
+		// Emoji support has come a long way, so this is no longer good advice: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/627
+		"vuejs-accessibility/accessible-emoji": "off",
+
 		// Enforce label tags have associated control, but only require either nesting, or explicit ID/for match, not both
 		"vuejs-accessibility/label-has-for": ["error",	{
 			"required": {
