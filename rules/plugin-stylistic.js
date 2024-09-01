c6d70119c3852cda330cf3d0facce57def7ac789
James Ross <james@jross.me>
2024-09-01 16:03:47 +0100

    fix: workaround stylistic plugin conflict
    https://github.com/eslint-stylistic/eslint-stylistic/issues/525

--- rules/plugin-stylistic.js
+++ rules/plugin-stylistic.js
@@ -215,6 +215,12 @@ module.exports = {
 		'@stylistic/type-annotation-spacing': ['error', {
 			before: false,
 			after: true,
+			overrides: {
+				operator: {
+					before: true,
+					after: true,
+				},
+			},
 		}],
 
 		// enforces consistent spacing inside TypeScript type generics.
